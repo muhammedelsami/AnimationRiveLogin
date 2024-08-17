23468baf93e4819837fefd4305b8e84524727c24
Muhammed Elşami <muhammed.elsami@detaysoft.com>
2024-08-17 17:00:15 +0300

    Initial commit

---
+++ AnimationRiveLogin/Assets/ToastView.swift
@@ -0,0 +1,18 @@
+//
+//  ToastView.swift
+//  AnimationRiveLogin
+//
+//  Created by Muhammed Elşami on 17.08.2024.
+//
+
+import SwiftUI
+
+struct ToastView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToastView()
+}
