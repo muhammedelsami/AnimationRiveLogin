3d202c4ecc169dcedf584682f3f9e0e2739ad508
Muhammed Elşami <muhammed.elsami@detaysoft.com>
2024-08-17 10:08:40 +0300

    Initial Commit

---
+++ AnimationRiveLogin/AnimationRiveLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationRiveLoginApp.swift
+//  AnimationRiveLogin
+//
+//  Created by Muhammed Elşami on 17.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationRiveLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
